4e09a9db536685ec37829c8ba4b618f682d52ad1
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-02-19 18:56:17 +0100

    adds: route path are added

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,17 @@
+import {Route, Routes } from 'react-router-dom'
+
+import Login from '../src/pages/Login.jsx'
+import Register from '../src/pages/Register.jsx'
+
 export default function App() {
 
   return (
     <>
-     <div className="flex flex-col justify-center text-center">
-      <h1 className="text-[32px] text-gray-500">Welcome to our small Herbarium</h1>
-      <p className="text-gray-600">This will be a Routing system</p>
-     </div>
+     <Routes>    
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+     </Routes>
     </>
-  )
+  );
 }
 
