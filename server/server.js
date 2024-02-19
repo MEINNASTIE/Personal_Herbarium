7bda0d4fdb29f360bb111f362eb0be4ebfd653ce
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2024-02-19 18:58:08 +0100

    adds: cors library is added

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import connectDB from './lib/mongo-db.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
-
+import cors from 'cors';
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 connectDB();
 app.use("/auth",authRoutes);
 
